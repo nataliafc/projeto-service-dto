37cc1f704010fa9c7e13f7d2b7b3ac4de21e40f5
Natalia Carvalho <nataliia.fc@gmail.com>
2021-11-10 14:44:48 -0300

    first

---
+++ src/main/java/com/serratec/backend/exercicio/ProjetoServiceDtoApplication.java
@@ -0,0 +1,13 @@
+package com.serratec.backend.exercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoServiceDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoServiceDtoApplication.class, args);
+	}
+
+}
